7e19e5db85b897ecb0284f7b99d7a9a81b8d601f
Mario <m.vongphachant@live.com>
2021-06-04 22:23:19 -0400

    commit

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Post } from '../post.model';
+import { PostsService } from '../posts.service';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent implements OnInit, OnDestroy {
+  isLoading = false;
+  posts: Post[] = [];
+  private postsSub: Subscription = new Subscription;
+
+  constructor(public postsService: PostsService) {}
+
+  ngOnInit() {
+    this.isLoading = true;
+    this.postsService.getPosts();
+    this.postsSub = this.postsService.getPostUpdateListener()
+      .subscribe((posts: Post[]) => {
+        this.isLoading = false;
+        this.posts = posts;
+        console.log(posts)
+      });
+  }
+
+  onDelete(postId: string) {
+    this.postsService.deletePost(postId)
+  }
+
+  ngOnDestroy() {
+    this.postsSub.unsubscribe();
+  }
+
+}
